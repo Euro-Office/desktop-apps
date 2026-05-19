2bfc0bbb6b682f65ea1ad33c20e7c22ba5f24758
konstantin.kireyev <konstantin.kireyev@onlyoffice.com>
2026-05-19 15:37:06 +0500

    feat: context-menu and dropdown-menu components

---
+++ common/loginpage/app/src/components/dropdown-menu/DropdownMenu.jsx
@@ -0,0 +1,42 @@
+import { Show, splitProps } from "solid-js";
+import { DropdownMenu as Primitive } from "@kobalte/core/dropdown-menu";
+import { cx } from "@knst/clsv";
+import "../context-menu/ContextMenu.styles.css";
+
+function Root(props) {
+  return <Primitive {...props} />;
+}
+
+function Trigger(props) {
+  return <Primitive.Trigger {...props} />;
+}
+
+function Content(props) {
+  const [, rest] = splitProps(props, ["class"]);
+  return (
+    <Primitive.Portal>
+      <Primitive.Content class={cx("menu", props.class)} {...rest} />
+    </Primitive.Portal>
+  );
+}
+
+function Item(props) {
+  const [, rest] = splitProps(props, ["class", "icon", "children"]);
+  return (
+    <Primitive.Item class={cx("menu-item", props.class)} {...rest}>
+      <Show when={props.icon}>
+        <svg class="menu-icon">
+          <use href={props.icon} />
+        </svg>
+      </Show>
+      {props.children}
+    </Primitive.Item>
+  );
+}
+
+function Separator(props) {
+  const [, rest] = splitProps(props, ["class"]);
+  return <Primitive.Separator class={cx("menu-separator", props.class)} {...rest} />;
+}
+
+export const DropdownMenu = Object.assign(Root, { Trigger, Content, Item, Separator });
