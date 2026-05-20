71512d6947dfc1791456b604a20629c7aa5dbc2e
SimplestStudio <Oleg.Kozhukharenko@onlyoffice.com>
2026-05-20 12:12:55 +0300

    [win-linux] debug after merge

--- win-linux/src/prop/cmainwindowimpl.cpp
+++ win-linux/src/prop/cmainwindowimpl.cpp
@@ -107,19 +107,6 @@ void CMainWindowImpl::refreshAboutVersion()
         if ( _lic_name.isEmpty() ) {
             _lic_name = DEFAULT_LICENSE_NAME;
             _lic_url = DEFAULT_LICENSE_URL;
-        } else if (_lic_name == "GNU AFFERO GENERAL PUBLIC LICENSE") {
-            QFile f(_lic_path);
-            if ( f.exists() ) {
-                if ( f.open(QIODevice::ReadOnly | QIODevice::Text )) {
-                    QTextStream stream(&f);
-                    QString n = stream.readLine();
-                    n = stream.readLine().trimmed();
-                    if (n.contains("Version 3")) {
-                        _lic_name = DEFAULT_LICENSE_NAME;
-                    }
-                    f.close();
-                }
-            }
         }
     } else if (_lic_name == "ONLYOFFICE Desktop Enterprise") {
         _lic_name = tr("License Agreement");
